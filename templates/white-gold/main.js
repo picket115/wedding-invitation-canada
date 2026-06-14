f941db923ad2179adcbe3553970b05025db9dc2a
PyoHyeIn <picket5774@gmail.com>
2026-06-14 17:20:29 +0900

    Apply vertical layout and horizontal separator to invitation section in White Gold template

--- templates/white-gold/main.js
+++ templates/white-gold/main.js
@@ -108,12 +108,12 @@ function buildInvitation(CFG) {
   el('invFamilies').innerHTML = `
     <div class="fam-block">
       <p class="fam-rel">${esc(CFG.groomFather)} · ${esc(CFG.groomMother)}의 아들</p>
-      <p class="fam-person">${esc(CFG.groomName)}</p>
+      <p class="fam-person">${CFG.groomName}</p>
     </div>
     <div class="fam-sep"></div>
     <div class="fam-block">
       <p class="fam-rel">${esc(CFG.brideFather)} · ${esc(CFG.brideMother)}의 딸</p>
-      <p class="fam-person">${esc(CFG.brideName)}</p>
+      <p class="fam-person">${CFG.brideName}</p>
     </div>`;
 }
 
